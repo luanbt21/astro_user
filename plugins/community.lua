a3cd36fdf774f76842afc153c18161b8fe08e7cd
luanbt21 <luanbt21@gmail.com>
2024-03-24 18:12:35 +0700

    codeium.vim instead codeium.nvim

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,5 +4,6 @@ return {
 
   { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.completion.codeium-vim" },
   { import = "astrocommunity.pack.rust" },
 }
--- plugins/user.lua
+++ plugins/user.lua
@@ -12,28 +12,28 @@ return {
   },
   -- { "Pocco81/auto-save.nvim", lazy = false },
   { "xiyaowong/transparent.nvim", lazy = false },
-  {
-    "Exafunction/codeium.nvim",
-    lazy = false,
-    dependencies = { "nvim-lua/plenary.nvim", "hrsh7th/nvim-cmp" },
-    config = function()
-      local lspkind = require "lspkind"
-      local cmp = require "cmp"
-      require("codeium").setup {
-        cmp.setup {
-          sources = {
-            { name = "codeium" },
-          },
-        },
-        formatting = {
-          format = lspkind.cmp_format {
-            mode = "symbol",
-            maxwidth = 50,
-            ellipsis_char = "...",
-            symbol_map = { Codeium = "" },
-          },
-        },
-      }
-    end,
-  },
+  -- {
+  --   "Exafunction/codeium.nvim",
+  --   lazy = false,
+  --   dependencies = { "nvim-lua/plenary.nvim", "hrsh7th/nvim-cmp" },
+  --   config = function()
+  --     local lspkind = require "lspkind"
+  --     local cmp = require "cmp"
+  --     require("codeium").setup {
+  --       cmp.setup {
+  --         sources = cmp.config.sources {
+  --           { name = "codeium" },
+  --         },
+  --       },
+  --       formatting = {
+  --         format = lspkind.cmp_format {
+  --           mode = "symbol",
+  --           maxwidth = 50,
+  --           ellipsis_char = "...",
+  --           symbol_map = { Codeium = "" },
+  --         },
+  --       },
+  --     }
+  --   end,
+  -- },
 }
